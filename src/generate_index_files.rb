57c48d3589e2ddc35d6f20f51da2829ccdaa9582
Lyubomir Anastasov <lubomir.anastasov@gmail.com>
2015-03-19 04:05:29 +0200

    rename

---
+++ src/generate_index_files.rb
@@ -0,0 +1,106 @@
+Dir.chdir ".."
+stack = []
+full_stack = []
+
+Dir.glob("*") { |x|
+	full_stack << x if x != "\n" && File.directory?("#{x}") 
+}
+
+def single_folder(stack)
+	Dir.foreach(".") { |file| 
+	  if file =~ /\w.jpg/ or file =~ /\w.png/
+		stack.push "#{file}"
+	  end
+	}
+end
+
+def multiple_folders(stack)
+	Dir.foreach(".") { |file| 
+	  if file =~ /\w.jpg/ or file =~ /\w.png/ && File.directory?("#{file}")
+		stack.push "#{file}"
+	  end
+	  if file =~ /chapter\d+|appendix\d+|bibliography|glossary|^index$|preface|content|introduction/ then
+	  	Dir.chdir "#{file}"
+	  	Dir.foreach(".") { |file| 
+	  	  if file =~ /\w.jpg/ or file =~ /\w.png/
+			stack.push "#{file}"
+		  end
+	  	}
+	  	generate_index_html(stack)
+	  	Dir.chdir ".."
+	  	stack = []
+	  end
+	}
+end
+
+
+def generate_index_html(stack)
+  stack.sort!
+  File.open("index.html","w") do |x|
+
+		x.puts "<!DOCTYPE html>"
+		x.puts "<html>"
+		x.puts "	<head>"
+		x.puts "		<link rel=\"stylesheet\" type=\"text/css\" href=\"../src/style.css\">"
+		x.puts "		<script src=\"http://code.jquery.com/jquery-latest.js\"></script>"
+		x.puts "		<script src=\"../src/pages.js\"></script>"
+		
+		x.puts "	</head>"
+		x.puts "	<body>"
+ 		x.puts "		<div id=\"wrapper\">"
+		stack.length.times { |j|
+		x.puts " 	       <img src=\"#{stack[j]}\">"
+		}	
+		x.puts "		</div>"
+		x.puts "	</body>"
+		x.puts "</html>"
+  end	
+end
+
+def generate_index_page(stack)
+	stack.delete("src")
+	stack.delete("content")
+	stack.sort_by! { |item| item.to_s.split(/(\d+)/).map { |e| [e.to_i, e] } }
+	stack.length.times do |j|
+		if stack[0] =~ /appendix\d+|bibliography|glossary|^index$|preface/ then
+			stack.rotate!(1)
+		end
+	end
+	stack.unshift("content")
+	puts Dir.pwd
+	File.open("index.html","w") do |x|
+
+		x.puts "<!DOCTYPE html>"
+		x.puts "<html>"
+		x.puts "	<head>"
+		x.puts "		<link rel=\"stylesheet\" type=\"text/css\" href=\"src/style.css\">"
+		x.puts "		<script src=\"http://code.jquery.com/jquery-latest.js\"></script>"
+		x.puts "		<script src=\"src/pages.js\"></script>"
+		
+		x.puts "	</head>"
+		x.puts "	<body>"
+ 	
+		x.puts " <div id=\"chapters_content\">"
+		x.puts " 	<div id=\"chapters\">"
+		x.puts "		<span> Book Title </span>"
+		x.puts "        </br>"
+
+		
+		stack.map.with_index { |e, i|
+			x.puts "    <a href=\"#{e}/index.html\" class=\"click#{i}\"> #{i}: </a>" 
+			x.puts "        <br> <br>"
+		}
+
+		x.puts "	</div>"
+		x.puts "</div>"
+		x.puts "	</body>"
+		x.puts "</html>"
+  end	
+end
+
+# if executed from the src folder
+# and files are in ../chapter1 folder
+# Dir.chdir ".."
+multiple_folders(stack)
+generate_index_page(full_stack)
+
